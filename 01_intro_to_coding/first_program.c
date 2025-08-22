be58a608ee3303ed8a68f47647dcae8c94e3d11d
CalvinSalazar <csalazar1669@floridapoly.edu>
2025-08-22 14:36:00 +0000

    Created the first program

---
+++ 01_intro_to_coding/first_program.c
@@ -0,0 +1,24 @@
+/*
+
+Multi-line comment.
+08/20/2025
+Calvin Salazar
+The first program in C.
+
+*/
+
+// One line comment
+
+// Preprocessor directive 
+// stdio.h - standard input output library 
+// .h - header file
+#include <stdio.h> 
+
+// Create the main function
+int main(void) {
+    printf("Hello\nWorld!\n"); // \n (new line) - escape sequence
+
+    printf("I\nam\nready\nto\ncode!\n");
+
+    return 0;
+}
