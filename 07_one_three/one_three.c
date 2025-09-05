4f46014d023ffeda1b15ea78bdf08125b5f28057
CalvinSalazar <csalazar1669@floridapoly.edu>
2025-09-05 14:16:52 +0000

    Lessons and homework.

---
+++ 07_one_three/one_three.c
@@ -0,0 +1,30 @@
+/*
+Practice with void functions
+*/
+
+#include <stdio.h>
+
+// Function prototypes = small description of a function
+void two(void){
+    printf("two");
+}
+void one_three(void){
+    printf("one\n %d\n three\n, two");
+}
+
+int main(void) {
+
+    printf("starting now:\n");
+
+
+    return 0;
+}
+
+// Function implementations
+void two(void) {
+
+}
+
+void one_three(void){
+
+}
---
+++ 08_homework/homework.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Welcome to \"COP 2271\"\nAssignment 2");
+
+    printf("\nLet's CODE\\Program!\n");
+}
