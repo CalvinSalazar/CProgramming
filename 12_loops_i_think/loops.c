ca5766e14f49afbd9f9114c59a0bfdf001ed3799
CalvinSalazar <csalazar1669@floridapoly.edu>
2025-09-17 14:07:59 +0000

    Working with loops in class.

---
+++ 12_loops_i_think/loops.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void countDown (int n);
+int n = 0;
+int i = 0;
+
+void printNRandom(int n);
+
+int main (void){
+    scanf("%d", &n);
+    countDown (n);
+    printNRandom (n);
+}
+
+void countDown (int n){
+while (n > 0){
+    printf("%d\n", n);
+    n = n - 1;
+    usleep(40000);
+}}
+
+void printNRandom(int n){
+while (i < n){
+    printf("%d", rand());
+    i = i + 1;
+}
+}
